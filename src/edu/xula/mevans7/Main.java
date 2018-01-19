2d72694cb294fce905f91fab9331e96fb8db5262
MarcusEvans <marcusevans03@gmail.com>
2018-01-19 10:44:57 -0600

    Updated and Added Array List enter Story ID153893058

--- src/edu/xula/mevans7/Main.java
+++ src/edu/xula/mevans7/Main.java
@@ -1,5 +1,7 @@
 package edu.xula.mevans7;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,11 +10,21 @@ public class Main {
         System.out.println("Welcome\n");
 
         Scanner sc = new Scanner(System.in);
+        //TODO Input User's Name
         System.out.println("What is your Name???\n");
         String name = sc.next();
-        //
-
+        //TODO Output User's Name with Message
         System.out.println("\nHello " + name.toUpperCase() +"\n");
+        //TODO Initialized Array List
+        ArrayList<Integer> absences = new ArrayList<>();
+        for (int i = 0; i < name.length(); i++) {
+            Random rand = new Random();
+            int number = rand.nextInt(10)+1;
+            absences.add(number);
+        }
+        //TODO Output Array
+        System.out.println(absences);
+
 
     }
 }
