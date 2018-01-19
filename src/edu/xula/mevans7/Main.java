0b5e65610110578c06a08cfce83ef73031d2b697
MarcusEvans <marcusevans03@gmail.com>
2018-01-19 10:21:37 -0600

    Story ID153893058

--- src/edu/xula/mevans7/Main.java
+++ src/edu/xula/mevans7/Main.java
@@ -3,7 +3,7 @@ package edu.xula.mevans7;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Attendance App\n");
-        
+        System.out.println("Welcome\n");
+
     }
 }
