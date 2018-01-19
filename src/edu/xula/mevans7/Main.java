3eedaca456f05671a392641590de6c27414a2b02
MarcusEvans <marcusevans03@gmail.com>
2018-01-19 10:30:58 -0600

    Story ID153893058

--- src/edu/xula/mevans7/Main.java
+++ src/edu/xula/mevans7/Main.java
@@ -1,9 +1,17 @@
 package edu.xula.mevans7;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome\n");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your Name???\n");
+        String name = sc.next();
+
+        System.out.println("\nHello " + name.toUpperCase() +"\n");
 
     }
 }
